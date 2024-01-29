052277e035d3e387008ca01a14b88fe933d45092
hollisyang <hollis2002@qq.com>
2024-01-29 13:02:34 +0800

    配置element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-
 </script>
 
 <template>
-  main
+  <el-button type="primary">
+    <el-icon :size="20">
+      <Edit />
+    </el-icon>i am button
+  </el-button>
   <RouterView></RouterView>
 </template>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
